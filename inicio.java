cdc5410b6e6d9ccb71cb729deabb97fea56e4b3c
Deiruwu <deirdre_uwu@hotmail.com>
2024-02-05 13:26:53 -0600

    Se actualiza

--- inicio.java
+++ inicio.java
@@ -9,7 +9,26 @@ public class inicio {
         for (int i = 0; i < usuario.length; i++) {
             System.out.println("Ingrese los datos de la persona " + (i + 1) + ":");
             usuario[i] = getters.crearObjeto(sc); //Llama al metodo que nos solicitará los datos para crear un objeto
+            System.out.println();
         }
+
+        // Crear usuario11 usando el constructor copia
+        persona usuario11 = new persona(usuario[0]);
+
+        System.out.println("¿Desea corregir los datos de algún objeto? (S/N)");
+        char cambio = sc.nextLine().toUpperCase().charAt(0);
+
+        while (cambio == 'S') {
+            System.out.print("Ingrese el número de la persona que desea corregir (1-5): ");
+            int numeroPersona = sc.nextInt();
+            sc.nextLine();
+            getters.corregirDatos(numeroPersona, usuario[numeroPersona-1], sc);
+            System.out.println("Datos corregidos:");
+            usuario[numeroPersona - 1].mostrarInformacion(numeroPersona);
+
+            System.out.println("¿Desea continuar corrigiendo los datos de algún objeto? (S/N)");
+            cambio = sc.nextLine().toUpperCase().charAt(0);
+}
 
         System.out.println("¿Cómo deseas previsualizar los datos?");
         boolean ordenarPorEdad;
@@ -32,24 +51,6 @@ public class inicio {
 
         persona.ordenamiento(usuario, ordenarPorEdad, ordenarMayorAMenor);
 
-
-        // Crear usuario11 usando el constructor copia
-        persona usuario11 = new persona(usuario[0]);
-
-        System.out.println("¿Desea corregir los datos de algún objeto? (S/N)");
-        char cambio = sc.nextLine().toUpperCase().charAt(0);
-        while (cambio == 'S') {
-
-            System.out.print("Ingrese el número de la persona que desea corregir (1-5): ");
-            int numeroPersona = sc.nextInt();
-            sc.nextLine();
-            getters.corregirDatos(numeroPersona, usuario[numeroPersona-1], sc);
-            System.out.println("Datos corregidos:");
-            usuario[numeroPersona - 1].mostrarInformacion(numeroPersona);
-
-            System.out.println("¿Desea continuar corrigiendo los datos de algún objeto? (S/N)");
-            cambio = sc.nextLine().toUpperCase().charAt(0);
-}
         System.out.println("*--------------------------------------------*");
         //ciclo para mostrar datos
         for (int j = 0; j < usuario.length; j++) {
